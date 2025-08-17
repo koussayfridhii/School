18efd41bf33b6618a8683c1a101c88636639c017
koussayfridhii <koussayfridhi.kf@gmail.com>
2025-08-17 21:43:49 +0300

    Refactor code structure for improved readability and maintainability

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": "/src", // This will point '@' to the src folder
+    },
+  },
+  server: {
+    port: 3000, // Frontend runs here
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // Backend API
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+  assetsInclude: ["**/*.glb"], // Allow importing .glb files
+});
